d73ffc997aa900a9a25dc66254e9bfa4f0db078b
Mukul <mukulliddlan04@gmail.com>
2025-03-03 21:21:56 +0530

    Add files via upload

---
+++ LIBRARY.py
@@ -0,0 +1,57 @@
+'''
+                                                                                                     #BUIT_IN_LIBRARY#
+built in methods/function
+math library
+sqrt,pow,cell,floor,fabs,factorial,pi,
+log,exp,sin,cos,tan,cot,cosh,sinh,tanh,etc
+
+import math
+print(math.sqrt(9))
+#import math
+#from math import sqrt,pow
+#print(math.fabs(5-10)) ->always positive value
+#import math as m
+print(m.sqrt(9))
+
+                                                                                             ##RANDOM LIBRARY##
+
+
+
+random(),randint()
+import random
+li=[]
+count=0
+while(len(li)!=5):
+    count+=1
+    num=random.randint(1,5)
+    if(num not in li):
+        li.append(num)
+print(li)
+print(count)
+                                                                                                  #DATETIME LIBRARY
+import datetime
+time=datetime.datetime.now()
+print(time)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
